e3a7792dde14aa3a69031fb1f0b6e6384c78e454
Heritier NTAGANIRA <30772923+Ntaganira@users.noreply.github.com>
2026-02-19 17:08:03 +0200

    Project Initialization

---
+++ src/main/java/com/bgc/event/BgcEventManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bgc.event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgcEventManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgcEventManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bgc/event/BgcEventManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgc.event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgcEventManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
